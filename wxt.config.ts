6112f0662de00342819e54bb868abbbc78e3c9d3
Lucas Leite <leitelucas470@gmail.com>
2026-03-11 10:40:35 -0300

    Adicionado dependência para gerar o ícone da extensão

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
+  modules: ['@wxt-dev/module-vue', '@wxt-dev/auto-icons'],
+  autoIcons: {
+    baseIconPath: 'assets/icon.svg'
+  }
 });
